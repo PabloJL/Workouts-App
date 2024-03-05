42d32dc8e843dbf58556c133dec906927a128a2b
PabloJL <jup_jim_7@hotmail.com>
2024-03-05 16:16:54 -0600

    Setup expo router

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, FlatList } from "react-native";
-import exercises from "./assets/data/exercises.json";
-import ExcerciseListItem from "./src/components/ExcerciseListItem";
+import exercises from "../../assets/data/exercises.json";
+import ExcerciseListItem from "../../src/components/ExcerciseListItem";
 
 export default function App() {
   return (
-    <View className=" flex-1 justify-center bg-gray-100 p-3 mt-20">
+    <View className=" flex-1 justify-center bg-gray-100 p-3">
       <FlatList
         contentContainerStyle={{ gap: 10 }}
         data={exercises}
--- src/components/ExcerciseListItem.jsx
+++ src/components/ExcerciseListItem.jsx
@@ -2,10 +2,12 @@ import { Text, View } from "react-native";
 
 function ExcerciseListItem({ item }) {
   return (
-    <View className=" bg-white p-3 rounded-xl gap-1">
+    <View className=" bg-white p-3 rounded-xl gap-1 shadow-sm">
       <Text className=" text-xl font-medium">{item.name}</Text>
       <Text className=" text-gray-600">
-        {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+        <Text className=" capitalize">
+          {item.muscle} | {item.equipment}
+        </Text>
       </Text>
     </View>
   );
