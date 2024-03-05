b6aea16d5c9f4ef75bc309fd31d0a03e08a826fc
PabloJL <jup_jim_7@hotmail.com>
2024-03-05 13:49:49 -0600

    Render one UI

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import exercises from "./assets/data/exercises.json";
 
 export default function App() {
+  const exercise = exercises[0];
+  // console.log(exercises);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className=" flex-1 justify-center bg-gray-100 p-3">
+      <View className=" bg-white p-3 rounded-xl gap-1">
+        <Text className=" text-xl font-medium">{exercise.name}</Text>
+        <Text className=" text-gray-600">
+          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+        </Text>
+      </View>
+
+      <StatusBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
